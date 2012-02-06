de97f97eeb097f0b06571b1da04efdea830104c8
shurailine <none@none>
2012-02-06 20:22:13 +0400

    JemmyFXBrowser

---
+++ tools/Jemmy/JemmyFXBrowser/src/org/jemmy/fx/Browser.java
@@ -0,0 +1,185 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package org.jemmy.fx;
+
+import java.awt.AWTException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import org.jemmy.action.GetAction;
+import org.jemmy.browser.BrowserDescriptor;
+import org.jemmy.browser.HierarchyDescriptor;
+import org.jemmy.browser.HierarchyView;
+import org.jemmy.control.Wrap;
+import org.jemmy.control.Wrapper;
+import org.jemmy.env.Environment;
+import org.jemmy.env.Timeout;
+import org.jemmy.lookup.ControlList;
+import org.jemmy.timing.State;
+import org.jemmy.timing.Waiter;
+
+/**
+ *
+ * @author shura
+ */
+public class Browser {
+
+    public static void main(String[] args) throws AWTException {
+        startApp(args);
+        runBrowser();
+    }
+
+    /**
+     * Register FX Browser on Scene using default key binding Ctrl-Shift-B
+     *
+     * @param scene
+     */
+    public static void register(Scene scene) {
+        register(scene, defaultKeyBind);
+    }
+
+    public static void register(Scene scene, final KeyBind kb) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            boolean browserStarted = false;
+
+            public void handle(KeyEvent ke) {
+                if (!browserStarted && kb.check(ke)) {
+                    browserStarted = true;
+                    Platform.runLater(new Runnable() {
+
+                        public void run() {
+                            try {
+                                runBrowser();
+                            } catch (AWTException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        String kbName = kb.getName();
+        if (kbName != null && kbName.length() > 0) {
+            System.err.println("Click " + kbName + " to run FX Browser.");
+        }
+    }
+
+    public static abstract class KeyBind {
+
+        public abstract boolean check(KeyEvent ke);
+
+        public abstract String getName();
+    };
+    private static KeyBind defaultKeyBind = new KeyBind() {
+
+        @Override
+        public boolean check(KeyEvent ke) {
+            return ke.isControlDown() && ke.isShiftDown() && ke.getCode() == KeyCode.B;
+        }
+
+        @Override
+        public String getName() {
+            return "Ctrl-Shift-B";
+        }
+    };
+
+    public static void runBrowser() throws AWTException {
+        try {
+            javax.swing.UIManager.setLookAndFeel(new MetalLookAndFeel());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(HierarchyView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        BrowserDescriptor descr = new BrowserDescriptor() {
+
+            ControlList hierarchy = new SceneList();
+            Wrapper wrapper = new SceneWrapper(Environment.getEnvironment());
+
+            @Override
+            public String getTitle() {
+                return "FX hierarchy";
+            }
+
+            @Override
+            public HierarchyDescriptor getSubHierarchyDescriptor(final Wrap wrap) {
+                return new HierarchyDescriptor() {
+
+                    Parent parent;
+
+                    {
+                        parent = new GetAction<Parent>() {
+
+                            @Override
+                            public void run(Object... parameters) throws Exception {
+                                setResult(((SceneWrap<? extends Scene>) wrap).getControl().getRoot());
+                            }
+                        }.dispatch(wrap.getEnvironment());
+                    }
+                    NodeHierarchy hierarchy = new NodeHierarchy(parent, Environment.getEnvironment());
+                    NodeWrapper wrapper = new NodeWrapper(((SceneWrap<? extends Scene>) wrap).getControl(),
+                            Environment.getEnvironment());
+
+                    @Override
+                    public ControlList getHierarchy() {
+                        return hierarchy;
+                    }
+
+                    @Override
+                    public Wrapper getWrapper() {
+                        return wrapper;
+                    }
+
+                    @Override
+                    public HierarchyDescriptor getSubHierarchyDescriptor(Wrap wrap) {
+                        return null;
+                    }
+                };
+            }
+
+            @Override
+            public ControlList getHierarchy() {
+                return hierarchy;
+            }
+
+            @Override
+            public Wrapper getWrapper() {
+                return wrapper;
+            }
+        };
+        new HierarchyView(descr).setVisible(true);
+    }
+
+    public static void startApp(final String[] argv) {
+        new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    AppExecutor.executeReflect(argv);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Browser.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }, "FX app launch thread").start();
+
+        new Waiter(new Timeout("launch start waiter", 10000)).ensureState(new State<Boolean>() {
+            public Boolean reached() {
+                try {
+                    Thread.sleep(100); // otherwise mac doesn't start
+                } catch (InterruptedException ex) {
+                }
+                Scene scene = Root.ROOT.lookup(new ByWindowType(Stage.class)).lookup(Scene.class).wrap(0).getControl();
+                return scene.getWindow().isShowing() ? Boolean.TRUE : null;
+            }
+        });
+    }
+}
---
+++ tools/Jemmy/JemmyFXBrowser/test/org/jemmy/client/TestBrowser.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jemmy.client;
+
+import org.jemmy.fx.Browser;
+import java.awt.AWTException;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author shura
+ */
+public class TestBrowser extends Application {
+    public static void main(final String[] args) throws AWTException {
+        new Thread(new Runnable() {
+
+            public void run() {
+                launch(TestBrowser.class, args);
+            }
+        }).start();
+        Browser.runBrowser();
+    }
+
+    protected Label lbl = new Label();
+
+    @Override
+    public void start(Stage stg) throws Exception {
+        Scene scene = new Scene(new Group());
+        VBox root = new VBox();
+        scene.setRoot(root);
+
+        MenuButton menu_button = new MenuButton("MenuButton");
+        addMenus(menu_button.getItems());
+        root.getChildren().add(menu_button);
+
+        MenuBar bar = new MenuBar();
+        addMenus(bar.getMenus());
+        root.getChildren().add(bar);
+        
+        Group grp = new Group();
+        grp.setLayoutX(0);
+        grp.setLayoutY(0);
+        HBox hBox = new HBox();
+        grp.getChildren().add(hBox);
+        Button btn = new Button();
+        btn.setText("push me");
+        lbl.setText("not yet pushed");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent t) {
+                lbl.setText("button pushed!");
+            }
+        });
+        hBox.getChildren().add(btn);
+        hBox.getChildren().add(lbl);
+        root.getChildren().add(grp);
+        
+        
+        stg.setScene(scene);
+        stg.setWidth(300);
+        stg.setHeight(300);
+        stg.show();
+    }
+    protected void addMenus(ObservableList list) {
+        Menu menu0 = new Menu("menu0");
+        MenuItem item0 = new MenuItem("item0");
+        item0.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent t) {
+                lbl.setText("item0 pushed!");
+            }
+        });
+        menu0.getItems().add(item0);
+        list.add(menu0);
+
+        Menu menu1 = new Menu("menu1");
+        Menu sub_menu1 = new Menu("sub-menu1");
+        Menu sub_sub_menu1 = new Menu("sub-sub-menu1");
+        MenuItem item1 = new MenuItem("item1");
+        item1.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent t) {
+                lbl.setText("item1 pushed!");
+            }
+        });
+        sub_menu1.getItems().add(sub_sub_menu1);
+        sub_sub_menu1.getItems().add(item1);
+        menu1.getItems().add(sub_menu1);
+        list.add(menu1);
+
+        Menu menu2 = new Menu("menu2");
+        menu2.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent t) {
+                lbl.setText("menu2 pushed!");
+            }
+        });
+        list.add(menu2);
+    }
+}
