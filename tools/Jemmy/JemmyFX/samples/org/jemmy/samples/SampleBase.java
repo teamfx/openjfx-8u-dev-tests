02a285a7a5ecd109ea9c5d23fdef20d3dd3dbf00
Alexandre (Shura) Iline <alexandre.iline@sun.com>
2012-06-27 17:07:18 +0400

    missed class

---
+++ tools/Jemmy/JemmyFX/samples/org/jemmy/samples/SampleBase.java
@@ -0,0 +1,29 @@
+package org.jemmy.samples;
+
+import javafx.application.Application;
+import org.jemmy.fx.AppExecutor;
+import org.jemmy.input.AWTRobotInputFactory;
+
+
+/**
+ *
+ * @author shura
+ */
+public class SampleBase {
+    
+    private static final String osName = System.getProperty("os.name").toLowerCase();;
+
+    static {
+        if(osName.contains("mac os")) {
+            AWTRobotInputFactory.runInOtherJVM(true);
+        }
+    }
+    
+    protected static void startApp(Class<? extends Application> app) throws InterruptedException {
+        AppExecutor.executeNoBlock(app);
+        if(osName.contains("mac os")) {
+            Thread.sleep(1000);
+        }
+    }
+    
+}
