b53614b09d5fe9f370a5d6aac908a64f035753f8
arusakov <none@none>
2016-06-17 12:40:19 +0300

    8158784: [TEST BUG] Need test for JDK-8157398 Summary: Test check whether node graphic is hidden after node collapsed. Reviewed-by: jgiles

--- functional/ControlsTests/test/javafx/scene/control/test/treetable/CommonTreeTableTest.java
+++ functional/ControlsTests/test/javafx/scene/control/test/treetable/CommonTreeTableTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,16 +24,77 @@
  */
 package javafx.scene.control.test.treetable;
 
-import javafx.scene.control.test.util.UtilTestFunctions;
-import org.junit.BeforeClass;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.control.test.tableview.ApplicationInteractionFunctions;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import org.jemmy.control.Wrap;
+import org.jemmy.env.Timeout;
+import org.jemmy.image.AWTImage;
+import org.jemmy.timing.Waiter;
+import org.junit.Test;
+import static com.sun.javafx.application.PlatformImpl.runAndWait;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.geometry.Bounds;
+import org.jemmy.fx.Root;
 
 /**
- * @author Alexander Kirov
+ * @author Andrey Rusakov
  */
-public class CommonTreeTableTest extends UtilTestFunctions {
+public class CommonTreeTableTest extends ApplicationInteractionFunctions {
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        TreeTableNewApp.main(null);
+    static {
+        isTableTests = false;
+    }
+
+    private static final double COLOR_TRESHOLD = 0.01;
+    private static final long COLOR_TIMEOUT = 3000l;
+
+    //Yes. Jemmy really uses RBGA, not RGBA (see AWTImage.getSupported)
+    private static double getColorDistance(Color fxColor, double[] rbg) {
+        return Math.sqrt((fxColor.getRed() - rbg[0]) * (fxColor.getRed() - rbg[0])
+                + (fxColor.getGreen() - rbg[2]) * (fxColor.getGreen() - rbg[2])
+                + (fxColor.getBlue() - rbg[1]) * (fxColor.getBlue() - rbg[1]));
+    }
+
+    private void waitForColor(Wrap wrap, int x, int y, Color expectedColor) {
+        new Waiter(new Timeout("Waiting for pixel to be " + expectedColor, COLOR_TIMEOUT))
+                .ensureState(() -> {
+                    AWTImage image = (AWTImage) wrap.getScreenImage();
+                    double[] colors = new double[4];
+                    image.getColors(x, y, colors);
+                    double dist = getColorDistance(expectedColor, colors);
+                    return dist < COLOR_TRESHOLD ? true : null;
+                });
+    }
+
+    /**
+     * 8158784: [TEST BUG] Need test for JDK-8157398
+     */    
+    @Test
+    public void testCollapsedNodeGraphicVisible() throws Exception {
+        Color nodeColor = new Color(Math.random(), Math.random(), Math.random(), 1);
+        TreeTableView treeTable = (TreeTableView) testedControl.getControl();
+        TreeItem<String> root = new TreeItem<>("Root Node");
+        TreeItem<String> child = new TreeItem<>("Child", new Rectangle(10, 10, nodeColor));
+        TreeTableColumn<String, String> column = new TreeTableColumn<>("Column");
+        column.setCellValueFactory(item -> new SimpleStringProperty("Whatever"));
+        treeTable.getColumns().setAll(column);
+        root.getChildren().add(child);
+        root.setExpanded(true);
+        treeTable.setShowRoot(true);
+        runAndWait(() -> {
+            treeTable.setRoot(root);
+        });
+        Wrap nodeGraphic = parent.lookup(node -> node == child.getGraphic()).wrap();
+        Wrap sceneWrap = Root.ROOT.lookup().wrap();
+        waitForColor(nodeGraphic, 1, 1, nodeColor);
+        Bounds graphicBounds = child.getGraphic().localToScene(child.getGraphic().getBoundsInLocal());
+        root.setExpanded(false);
+        waitForColor(sceneWrap,
+                (int) (graphicBounds.getMinX() + graphicBounds.getWidth() / 2),
+                (int) (graphicBounds.getMinY() + graphicBounds.getHeight() / 2), Color.WHITE);
     }
 }
